9b354030181bf711d4c1633d94ee4c477157c28c
Chris <Chris@Cag1080.fritz.box>
2018-07-04 22:43:15 +0200

    Wert verändert

--- src/neuralNetwork/ConstantBias.java
+++ src/neuralNetwork/ConstantBias.java
@@ -5,7 +5,7 @@ public class ConstantBias implements BiasFiller
 	// Computes a initial constant bias 
 	public float compute(int currWeight, int in, int out) 
 	{
-		return 0.005f; //*currWeight*in*out;
+		return 0.5f; //*currWeight*in*out;
 	}
 
 }
