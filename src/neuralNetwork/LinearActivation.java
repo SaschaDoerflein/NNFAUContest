47a4ef920bf9eb68bbf87efd174011d1d7f431c9
Chris <Chris@Cag1080.fritz.box>
2018-07-05 16:32:12 +0200

    add constructor for scale-factor

--- src/neuralNetwork/LinearActivation.java
+++ src/neuralNetwork/LinearActivation.java
@@ -5,9 +5,22 @@ package neuralNetwork;
 
 public class LinearActivation implements ActivationFunction 
 {
+	float factor;
+	float add;
+	boolean use = false;
+	public LinearActivation(float factor, float add) {
+		this.factor = factor;
+		this.add = add;
+		this.use = true;
+	}
+	//no changes need if you wont use factor
+	public LinearActivation() {
+		//this.factor = factor;
+	}
 	// This function computes linear(x) and returns it
 	public float compute(float x)
 	{
+		if (use) return factor*x+add;
 		return x;
 	}
 
