d0fd69b4131300a71c07e5c4e604626051e80248
saschaluebke <sascha.luebke@web.de>
2018-07-06 08:19:08 +0200

    Brute force implementation but there is a bug!!!

--- src/neuralNetwork/MainSascha.java
+++ src/neuralNetwork/MainSascha.java
@@ -11,180 +11,52 @@ public class MainSascha
 {
 	public static void main(String[] args) throws IOException
 	{
-		TestHelper th = new TestHelper(new SimpleAccuracyFunction());
+		SimpleAccuracyFunction saf = new SimpleAccuracyFunction();
+		TestHelper th = new TestHelper(saf);
 		
-		int[] test = {1,1,0};
+		//Define Variables for brute force
+		String nameOfTrainingsData = "titanic_training.txt";
+		
+		float minLearningRate = 0.00005f, maxLearningRate = 0.0001f, learningRateStepSize = 0.000005f;
+		
+		float minStartRate=1, maxStartRate=2, startRateStepSize=1f;
+		
+		int minIterations=100, maxIterations=1000, iterationsStepSize=10;
+		
+		float minFactor=1, maxFactor=2, factorStepSize=1;
+		
+		ArrayList<ActivationFunction> activationFunctions = new ArrayList<>();
+		activationFunctions.add(new LinearActivation());
+		activationFunctions.add(new SigmoidActivation());
+		activationFunctions.add(new TanhActivation());
+		
+		int minExcecutionPrevent = 16, maxExcecutionPrevent=17, excecutionPreventStepSize=1;
+		
+		int inputCount=6, outputCountRight=1;
+		int minOutputCountLeft =1, maxOutputCountLeft=3;
+		int minHiddenLayers=2, maxHiddenLayers=10;
+		int minHiddenCount=1, maxHiddenCount=32;
+		
+		ArrayList<LossFunction> lfList = new ArrayList<>();
+		lfList.add(new EuclideanLoss());
+		
+		ArrayList<WeightFiller> wfList = new ArrayList<>();
+		wfList.add(new RandomWeight());
+		
+		ArrayList<BiasFiller> bfList = new ArrayList<>();
+		bfList.add(new ConstantBias());
 		
 		
-		if (test[0]==1) {
-			int iterations = 1000;
-			int epochs = 1;
-			float learningRate = 0.0001f;
-			
-			//Here you can create more networks to see if there is a varianz in the output networks. 
-			Network[] networks = new Network[epochs];
-			
-			for(int i=0; i<epochs; i++) {
-				Network n=new Network(new VariantLearningRate(learningRate,iterations));
-				//Network n=new Network(new ConstantLearningRate(0.0001f));
+		th.bruteForceTrainingsRun(nameOfTrainingsData, 
+				minLearningRate, maxLearningRate, learningRateStepSize, 
+				minStartRate, maxStartRate, startRateStepSize, 
+				minIterations, maxIterations, iterationsStepSize, 
+				minFactor, maxFactor, factorStepSize, activationFunctions, 
+				inputCount, outputCountRight, 
+				minOutputCountLeft, maxOutputCountLeft, minHiddenLayers, maxHiddenLayers, minHiddenCount, maxHiddenCount, 
+				lfList, wfList, bfList,
+				minExcecutionPrevent, maxExcecutionPrevent, excecutionPreventStepSize);
 
-				n.add(new InputLayer(6));
-				
-				//In every epoch there are more FullyConnected Layers
-				for(int ii = 0; ii<i;ii++) {
-					n.add(new FullyConnected(new SigmoidActivation(), new RandomWeight(), new ConstantBias(), 6, 6));
-				}
-				
-				n.add(new OutputLayer(new EuclideanLoss(),new SigmoidActivation(), new RandomWeight(), new ConstantBias(), 6, 1));
-				networks[i] = n;
-			}
-			
-			
-			ArrayList<Datum> dataAndLabel = dataAndLabel = DataReader.readTitanicDataset("titanic_training.txt",true);
-
-			th.performTest(networks, iterations,dataAndLabel);
-			
-			/* TODO: prediction make no sense right now because we have no expected value
-			 * 
-			 * ArrayList<Datum> testData=DataReader.readTitanicDataset("titanic_test1.txt",false);
-	
-			for(int i=0;i<testData.size();i++)
-			{
-				Blob out[]=n.forward(testData.get(i).data);
-				if(out[out.length-1].getValue(0) < 0.5)
-				{
-					testData.get(i).label.setValue(0,0f);
-				}
-				else
-				{
-					testData.get(i).label.setValue(0,1f);
-				}
-			}
-	
-			DataWriter.writeLabelsToFile("titanic_prediction.txt", testData);
-			System.out.println("done");
-			 */
-
-		}
-		
-		if (test[1] == 1) {		
-			int iterations = 10000;
-			int epochs = 1;
-			float learningRate = 0.0000000007f;
-			
-		
-			
-			//Here you can create more networks to see if there is a varianz in the output networks. 
-			Network[] networks = new Network[epochs];
-			
-			for(int i=0; i<epochs; i++) {
-				Network n=new Network(new VariantLearningRate(learningRate,iterations));
-				//Network n=new Network(new ConstantLearningRate(0.0001f));
-
-				n.add(new InputLayer(26));
-				
-				for(int ii = 0; ii<0;ii++) {
-					n.add(new FullyConnected(new SigmoidActivation(), new RandomWeight(), new ConstantBias(), 6, 6));
-				}
-				
-				n.add(new OutputLayer(new EuclideanLoss(),new LinearActivation(), new RandomWeight(), new ConstantBias(), 26, 6));
-				networks[i] = n;
-			}
-			
-		
-			ArrayList<Datum> dataAndLabel = DataReader.readTraveltimeDataset("rta_training.txt",true);
-		
-		
-			
-			th.performTest(networks, iterations,dataAndLabel);
-	/*
-			ArrayList<Datum> testData2=DataReader.readTraveltimeDataset("rta_test1.txt",false);
-	
-			for(int i=0;i<testData2.size();i++)
-			{
-				Blob out[]=n2.forward(testData2.get(i).data);
-				if(out[out.length-1].getValue(0) < 0.5)
-				{
-					testData2.get(i).label.setValue(0,0f);
-				}
-				else
-				{
-					testData2.get(i).label.setValue(0,1f);
-				}
-			}
-
-			DataWriter.writeLabelsToFile("rta_prediction.txt", testData2);
-			*/
-		}
-		
-		
-		
-		/*---------------------------------------------------
-		------ Example NeuralNet using ImageDataset ------
-		--------------------------------------------------- */
-		
-		if (test[2] == 1) {		
-			int iterations = 1000;
-			int epochs = 1;
-			float learningRate = 0.0000001f;
-			
-			//Here you can create more networks to see if there is a varianz in the output networks. 
-			Network[] networks = new Network[epochs];
-			
-			for(int i=0; i<epochs; i++) {
-				Network n=new Network(new ConstantLearningRate(learningRate));
-				//Network n=new Network(new ConstantLearningRate(0.0001f));
-
-				n.add(new InputLayer(32*32*3));
-				
-				for(int ii = 0; ii<0;ii++) {
-					n.add(new FullyConnected(new SigmoidActivation(), new RandomWeight(), new ConstantBias(), 6, 6));
-				}
-				
-				n.add(new OutputLayer(new EuclideanLoss(), new LinearActivation(), new RandomWeight(), new ConstantBias(), 32*32*3, 1));
-				networks[i] = n;
-			}
-			
-			ArrayList<Datum> dataAndLabel = DataReader.getImageDataset("image_training.bin",true);
-			
-			th.performTest(networks,iterations,dataAndLabel);
-		
-			/*
-			 * ArrayList<Datum> testData3=DataReader.getImageDataset("image_test1.bin",false);
-	
-			for(int i=0;i<testData3.size();i++)
-			{
-				Blob out[]=n2.forward(testData3.get(i).data);
-				testData3.get(i).label.setValue(0, Math.round(out[out.length-1].getValue(0)));
-				/*if(out[out.length-1].getValue(0) < 0.5)
-				{
-					testData3.get(i).label.setValue(0,0f);
-				}
-				else
-				{
-					testData3.get(i).label.setValue(0,1f);
-				}
-			}
-	
-			DataWriter.writeLabelsToFile("image_prediction.txt", testData3);
-			 */
-			
-		}
 	}
-	
-	//Build all possible Networks
-	private Network[] BuildNetworks(int numInputLayers, int minMiddleLayers, int maxMiddleLAyers, int numOuputLayers,
-			ArrayList<ActivationFunction> afList, ArrayList<BiasFiller> bfiller, int maxIterations, int minIterations, int iterationStepSize) {
-		//Take all Learning rates
-
-		
-		
-		return null;
-		
-	}
-	
-	
-
-	
 	
 }
--- src/test/TestHelper.java
+++ src/test/TestHelper.java
@@ -1,13 +1,31 @@
 package test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import neuralNetwork.ActivationFunction;
+import neuralNetwork.BiasFiller;
 import neuralNetwork.Blob;
+import neuralNetwork.ConstantBias;
 import neuralNetwork.DataReader;
 import neuralNetwork.Datum;
+import neuralNetwork.EuclideanLoss;
+import neuralNetwork.FullyConnected;
+import neuralNetwork.InputLayer;
+import neuralNetwork.LinearActivation;
+import neuralNetwork.LossFunction;
 import neuralNetwork.Network;
+import neuralNetwork.OutputLayer;
+import neuralNetwork.RandomWeight;
+import neuralNetwork.SigmoidActivation;
+import neuralNetwork.TanhActivation;
+import neuralNetwork.VariantLearningRate;
+import neuralNetwork.WeightFiller;
 
 public class TestHelper {
 	private int count;
@@ -18,7 +36,7 @@ public class TestHelper {
 	}
 	
 	//Write every result from every iterations separated in epochs into a output file
-	public void performTest(Network[] networks, int iterations, ArrayList<Datum> dataAndLabel) {
+	public void performTests(Network[] networks, int iterations, ArrayList<Datum> dataAndLabel) {
 		count++;
 		System.out.println("------------Starting Test"+count+"------------");
 		float[] realErrors = new float[networks.length];
@@ -110,13 +128,238 @@ public class TestHelper {
 		}
 			
 		
-		/*
-		 * TODO: For test file? We can't compute any variance because we have no expected values!
-		 * 
-		 */
 	}
-
 	
-	
+	//Beware: (max-min)/StepSize must be even!
+	public void bruteForceTrainingsRun(String nameOfTrainingsData,
+			float minLearningRate, float maxLearningRate, float learningRateStepSize,
+			float minStartRate, float maxStartRate, float startRateStepSize,
+			int minIterations, int maxIterations, int iterationsStepSize, 
+			float minFactor, float maxFactor,float factorStepSize,
+			ArrayList<ActivationFunction> activationFunctions,
+			int inputCount, int outputCountRight, 
+			int minOutputCountLeft, int maxOutputCountLeft,
+			int minHiddenLayers, int maxHiddenLayers,
+			int minHiddenCount, int maxHiddenCount,
+			ArrayList<LossFunction> lfList, ArrayList<WeightFiller> wfList, ArrayList<BiasFiller> bfList,
+			int minExcecutionPrevent, int maxExcecutionPrevent, int excecutionPreventStepSize) {
+		
+		ArrayList<Float> result = af.getResult();
+		ArrayList<Float> expected = af.getExpected();
+		
+		//**Variables**
+		//You can compute how many loops the bruteForceTrainingRuns 
+		//Beware allCombinations shouldn't be greater than 2 hoch Integer.MaxInteger()
+		BigInteger learningRateCombinations =  BigInteger.valueOf((long) ((maxLearningRate - minLearningRate)/learningRateStepSize));
+		BigInteger startrateCombinations =  BigInteger.valueOf((long) ((maxStartRate - minStartRate)/startRateStepSize));
+		BigInteger iterationCombinations =  BigInteger.valueOf((long) ((maxIterations - minIterations)/iterationsStepSize));
+		BigInteger factorCombinations =  BigInteger.valueOf((long)((maxFactor - minFactor)/factorStepSize));
+		BigInteger outputCountLeftCombinations =  BigInteger.valueOf((long)(maxOutputCountLeft-minOutputCountLeft));
+		BigInteger hiddenLayersCombinations =  BigInteger.valueOf((long)maxHiddenLayers-minHiddenLayers);
+		BigInteger hiddenCountCombinations =  BigInteger.valueOf((long)maxHiddenCount*minHiddenCount);
+		BigInteger activationFunctionsCombination = BigInteger.valueOf((long)activationFunctions.size());
+		BigInteger excecutionPreventCombinations = BigInteger.valueOf((long)((maxExcecutionPrevent-minExcecutionPrevent)/excecutionPreventStepSize));
+		BigInteger lfListCombinations = BigInteger.valueOf((long)lfList.size());
+		BigInteger wfListCombinations = BigInteger.valueOf((long)wfList.size());
+		BigInteger bfListCombinations = BigInteger.valueOf((long)bfList.size());
+		
+		
+		BigInteger allCombinations = new BigInteger("1");
+		allCombinations = allCombinations.multiply(learningRateCombinations);
+		allCombinations = allCombinations.multiply(startrateCombinations);
+		allCombinations = allCombinations.multiply(iterationCombinations);
+		allCombinations = allCombinations.multiply(factorCombinations);
+		allCombinations = allCombinations.multiply(outputCountLeftCombinations);
+		allCombinations = allCombinations.multiply(hiddenLayersCombinations);
+		allCombinations = allCombinations.multiply(hiddenCountCombinations);
+		allCombinations = allCombinations.multiply(activationFunctionsCombination);
+		allCombinations = allCombinations.multiply(excecutionPreventCombinations);
+		allCombinations = allCombinations.multiply(lfListCombinations);
+		allCombinations = allCombinations.multiply(wfListCombinations);
+		allCombinations = allCombinations.multiply(bfListCombinations);
 
+		
+		BigInteger thisCombination = new BigInteger("1");
+		final BigInteger oneBigInteger = new BigInteger("1");
+		
+		//Get Data
+		try {
+			ArrayList<Datum> dataAndLabel = DataReader.readTitanicDataset(nameOfTrainingsData,true);
+			int length = dataAndLabel.size();
+			
+			//while(!allCombinations.equals(thisCombination)) {
+				//System.out.println(thisCombination);
+				
+				for(float currentLearningRate = minLearningRate; currentLearningRate < maxLearningRate; currentLearningRate += learningRateStepSize) {
+					for(float currentStartRate = minStartRate; currentStartRate < maxStartRate; currentStartRate += startRateStepSize) {
+						for(int currentIteration = minIterations; currentIteration < maxIterations; currentIteration += iterationsStepSize) {
+							for(float currentFactor = minFactor; currentFactor < maxFactor; currentFactor += factorStepSize) {
+								for(int currentActivationFunction = 0; currentActivationFunction < activationFunctions.size(); currentActivationFunction++) {
+									for(int currentExcecutionPrevent = minExcecutionPrevent; currentExcecutionPrevent< maxExcecutionPrevent; currentExcecutionPrevent += excecutionPreventStepSize) {
+									for(int currentOutputLeft = minOutputCountLeft; currentOutputLeft < maxOutputCountLeft; currentOutputLeft++) {
+										for(int currentHiddenLayer = minHiddenLayers; currentHiddenLayer < maxHiddenLayers; currentHiddenLayer++) {
+											for(int currentlfList = 0; currentlfList < lfList.size(); currentlfList++) {
+												for(int currentwfList = 0; currentwfList < wfList.size(); currentwfList++) {
+													for(int currentbfList = 0; currentbfList < bfList.size(); currentbfList++) {
+														
+														
+															//Combinations for hidden layer...
+														int[] hiddenConnections = null;
+
+														if(minHiddenLayers > 0) {
+															hiddenConnections = new int[currentHiddenLayer-1];
+															for(int i = 0; i < hiddenConnections.length; i++) {
+																hiddenConnections[i] = minHiddenCount;
+															}
+																int pivotElement = 0;
+																
+																while(hiddenConnections[hiddenConnections.length-1]< maxHiddenCount) {
+																	while(hiddenConnections[pivotElement] < maxHiddenCount) {
+																		
+																		
+																		//Set Objects
+																		LossFunction lossFunction = lfList.get(currentlfList);
+																		WeightFiller weightFiller = wfList.get(currentwfList);
+																		BiasFiller biasFiller = bfList.get(currentbfList);
+																		
+																		ActivationFunction activationFunction = activationFunctions.get(currentActivationFunction);
+																		VariantLearningRate variantLearningRate = new VariantLearningRate(currentStartRate,currentIteration,currentFactor,activationFunction);
+																		
+																		
+																		//Build Network
+																		Network network = new Network(variantLearningRate);
+
+																		network.add(new InputLayer(inputCount));
+																		
+																		for(int hidden=1; hidden<currentHiddenLayer; hidden++) {
+																			//Just when in and out are same number
+																			//You have #hidden-1 numbers of connections=#con    + input and output connection
+																			//there are #con over currentHidden-minHidden combinations
+																			if (hidden == 1) {
+																				network.add(new FullyConnected(activationFunction,weightFiller,biasFiller,inputCount,hiddenConnections[hidden-1],currentExcecutionPrevent));
+																			}else if (hidden == currentHiddenLayer-1) {
+																				network.add(new FullyConnected(activationFunction,weightFiller,biasFiller,hiddenConnections[hidden-1],outputCountRight,currentExcecutionPrevent));
+																			}else {
+																				network.add(new FullyConnected(activationFunction,weightFiller,biasFiller,hiddenConnections[hidden-1],hiddenConnections[hidden],currentExcecutionPrevent));
+																			}
+																			
+																		}
+																		
+																		network.add(new OutputLayer(lossFunction,activationFunction, weightFiller, biasFiller, currentOutputLeft, outputCountRight, currentExcecutionPrevent));
+																		
+																		
+																		//Perform Test
+																		
+																		for(int i = 0; i<length; i++) {
+																			Blob out=network.trainSimpleSGD(dataAndLabel.get(i).data, dataAndLabel.get(i).label);
+																			
+																			for(int h=0;h<out.getLength();h++)
+																			{
+																				result.add(out.getValue(h));
+																			}
+														
+																			for(int h=0;h<dataAndLabel.get(i).label.getLength();h++)
+																			{
+																				expected.add(dataAndLabel.get(i).label.getValue(h));
+																			}
+																			
+																		}
+																		
+																		float realError = af.computeRealError();
+																		float variance = af.computeVar();
+																		int[] errorClasses = af.computeSimpleErrorClasses();
+																		
+																		
+																		
+																		//Store Result
+																		StringBuilder sb = new StringBuilder();
+																		sb.append(realError+" "+variance+" ");
+																		
+																		for(int i=0;i<errorClasses.length;i++) {
+																			sb.append(errorClasses[i]+" ");
+																		}
+																		
+																		sb.append(currentHiddenLayer+" ");
+																		
+																		for(int i=0; i<hiddenConnections.length;i++) {
+																			sb.append(hiddenConnections[i]+" ");
+																		}
+																		
+																		sb.append(currentLearningRate+" ");
+																		sb.append(currentStartRate+" ");
+																		sb.append(currentIteration+" ");
+																		sb.append(currentFactor+" ");
+																		sb.append(currentActivationFunction+" ");
+																		sb.append(currentExcecutionPrevent+" ");
+																		sb.append(currentOutputLeft+" ");
+																		sb.append(currentlfList+" ");
+																		sb.append(currentwfList+" ");
+																		sb.append(currentbfList);//Don't forget the space
+																		
+																		try
+																		{
+																			File file = new File("results.txt");
+
+																			// if file doesnt exists, then create it 
+																			if ( ! file.exists( ) )
+																			{
+																				file.createNewFile( );
+																			}
+
+																			FileWriter fw = new FileWriter( file.getAbsoluteFile( ) );
+																			BufferedWriter bw = new BufferedWriter( fw );
+																			bw.write( sb.toString() );
+																			bw.close( );
+																		}
+																		catch( IOException e )
+																		{
+																			System.out.println("Error: " + e);
+																			e.printStackTrace( );
+																		}
+																		
+																		//clean
+																		af.setExpected(new ArrayList<Float>());
+																		af.setResult(new ArrayList<Float>());
+																		
+																		
+																		
+																		
+																		
+																		hiddenConnections[pivotElement]++;
+																	}
+																	pivotElement++;
+																	if(hiddenConnections.length < pivotElement) {
+																		for(int i=0; i<pivotElement;i++) {
+																			hiddenConnections[pivotElement] = minHiddenCount;
+																		}
+																		pivotElement = 0;
+																	}
+																}
+															}else {
+																hiddenConnections = new int[0];
+															}
+																
+																
+																	
+															}
+																
+														}
+														
+														
+													}
+												}
+											}
+										}
+									}
+									}
+								}
+							}
+						}
+					}
+	 catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+				}
+	
 }
