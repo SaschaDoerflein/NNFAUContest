0b2242f9ff666aaf6b529cec57da9ba3268a78ad
Chris <Chris@Cag1080.fritz.box>
2018-07-08 20:47:27 +0200

    not important change

--- src/neuralNetwork/RandomWeight.java
+++ src/neuralNetwork/RandomWeight.java
@@ -5,7 +5,7 @@ public class RandomWeight implements WeightFiller {
 	// Computes random numbers for weight initialization
 	public float compute(int currWeight, int inSize, int outSize)
 	{
-		return (float) (Math.random()-0.5);
+		return (float) ((Math.random()-0.5));
 	}
 
 }
