d5695c9c1a75a6b893296ecc644439434dd1152b
Chris <Chris@Cag1080.fritz.box>
2018-07-08 20:43:03 +0200

    add lstm training

--- src/neuralNetwork/Network.java
+++ src/neuralNetwork/Network.java
@@ -61,6 +61,39 @@ public class Network
 
 	   return forward[forward.length-1];
    }
+   
+   public Blob trainLSTM(Blob in, Blob out) {
+	   
+	   Blob[] forward0=new Blob[in.getLength()+out.getLength()];
+	   Blob[] forward=new Blob[in.getLength()+out.getLength()];
+	   Blob[] forward2=new Blob[in.getLength()+out.getLength()];
+	   Blob forward3 = new Blob(out.getLength());
+	   //forward[0].set=in.getValue(0);
+	   for(int i=0;i<in.getLength();i++)
+	   {
+		   forward[i] = new Blob(2);
+		   forward[i].setValue(0, in.getValue(i));
+		   forward0[i] = new Blob(1);
+		   forward0[i].setValue(0, in.getValue(i));
+	   }
+	   for(int i=0;i<out.getLength();i++)
+	   {
+		   forward[i+in.getLength()] = new Blob(2);
+		   forward[i+in.getLength()].setValue(0, out.getValue(i));
+		   forward0[i+in.getLength()] = new Blob(1);
+		   forward0[i+in.getLength()].setValue(0, out.getValue(i));
+	   }
+	   for(int i=0;i<in.getLength()+out.getLength()-1;i++)
+	   {
+		   forward2[i+1] = layers.get(0).forward(forward[i]);
+		   layers.get(0).backward(forward0[i+1], null);
+		   forward[i+1].setValue(1, forward2[i+1].getValue(0));
+	   }
+	   for (int i = 0; i < out.getLength(); i++) {
+		   forward3.setValue(i, forward2[i+in.getLength()].getValue(0));
+	   }
+	   return forward3;
+   }
 
 
     public Blob trainMinibatchSGD(Blob in, Blob out)
