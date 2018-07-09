810130e6700b88cd672a30a54b9ca506c89f7e6e
Chris <Chris@Cag1080.fritz.box>
2018-07-09 15:04:54 +0200

    train and test lstm

--- src/neuralNetwork/Network.java
+++ src/neuralNetwork/Network.java
@@ -62,6 +62,48 @@ public class Network
 	   return forward[forward.length-1];
    }
    
+   public Blob forwardLSTM(Blob in) {
+	   
+	   Blob[] forward0=new Blob[in.getLength()+6];
+	   Blob[] forward=new Blob[in.getLength()+6];
+	   Blob[] forward2=new Blob[in.getLength()+6];
+	   Blob forward3 = new Blob(6);
+	   //forward[0].set=in.getValue(0);
+	   float div = 3000;
+	   for(int i=0;i<in.getLength();i++)
+	   {
+		   forward[i] = new Blob(1);
+		   forward[i].setValue(0, in.getValue(i)/div);
+		   forward0[i] = new Blob(1);
+		   forward0[i].setValue(0, in.getValue(i)/div);
+	   }
+	   for(int i=0;i<6;i++)
+	   {
+		   forward[i+in.getLength()] = new Blob(1);
+		   //forward[i+in.getLength()].setValue(0, out.getValue(i)/div);
+		   forward0[i+in.getLength()] = new Blob(1);
+		   //forward0[i+in.getLength()].setValue(0, out.getValue(i)/div);
+	   }
+	   float currRate=learningRate.getLearningRate();
+	   for(int i=0;i<in.getLength()+6-1;i++)
+	   {
+		   forward2[i+1] = layers.get(0).forward(forward[i]);
+		   layers.get(0).backward(forward0[i+1], null);
+		   layers.get(0).updateWeightsAndBias(forward[i], currRate);
+		   forward0[i+1].setValue(0, forward2[i+1].getValue(0));
+	   }
+	   /*for (int i=in.getLength()-1;i<in.getLength()+6-1;i++) {
+		   forward2[i+1] = layers.get(0).forward(forward[i]);
+		   layers.get(0).backward(forward0[i+1], null);
+		   layers.get(0).updateWeightsAndBias(forward[i], currRate);
+		   forward[i+1].setValue(0, forward2[i+1].getValue(0));
+		   forward[i+1].setValue(1, forward2[i+1].getValue(0));
+	   }*/
+	   for (int i = 0; i < 6; i++) {
+		   forward3.setValue(i, forward0[i+in.getLength()].getValue(0)*div);
+	   }
+	   return forward3;
+   }
    public Blob trainLSTM(Blob in, Blob out) {
 	   
 	   Blob[] forward0=new Blob[in.getLength()+out.getLength()];
@@ -69,19 +111,20 @@ public class Network
 	   Blob[] forward2=new Blob[in.getLength()+out.getLength()];
 	   Blob forward3 = new Blob(out.getLength());
 	   //forward[0].set=in.getValue(0);
+	   float div = 3000;
 	   for(int i=0;i<in.getLength();i++)
 	   {
-		   forward[i] = new Blob(2);
-		   forward[i].setValue(0, in.getValue(i));
+		   forward[i] = new Blob(1);
+		   forward[i].setValue(0, in.getValue(i)/div);
 		   forward0[i] = new Blob(1);
-		   forward0[i].setValue(0, in.getValue(i));
+		   forward0[i].setValue(0, in.getValue(i)/div);
 	   }
 	   for(int i=0;i<out.getLength();i++)
 	   {
-		   forward[i+in.getLength()] = new Blob(2);
-		   forward[i+in.getLength()].setValue(0, out.getValue(i));
+		   forward[i+in.getLength()] = new Blob(1);
+		   forward[i+in.getLength()].setValue(0, out.getValue(i)/div);
 		   forward0[i+in.getLength()] = new Blob(1);
-		   forward0[i+in.getLength()].setValue(0, out.getValue(i));
+		   forward0[i+in.getLength()].setValue(0, out.getValue(i)/div);
 	   }
 	   float currRate=learningRate.getLearningRate();
 	   for(int i=0;i<in.getLength()+out.getLength()-1;i++)
@@ -89,10 +132,10 @@ public class Network
 		   forward2[i+1] = layers.get(0).forward(forward[i]);
 		   layers.get(0).backward(forward0[i+1], null);
 		   layers.get(0).updateWeightsAndBias(forward[i], currRate);
-		   forward[i+1].setValue(1, forward2[i+1].getValue(0));
+		   forward0[i+1].setValue(0, forward2[i+1].getValue(0));
 	   }
 	   for (int i = 0; i < out.getLength(); i++) {
-		   forward3.setValue(i, forward2[i+in.getLength()].getValue(0));
+		   forward3.setValue(i, forward0[i+in.getLength()].getValue(0)*div);
 	   }
 	   return forward3;
    }
