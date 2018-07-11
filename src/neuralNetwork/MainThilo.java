272d5d1b44ae338f55d588899417d88b6d12a05a
saschaluebke <sascha.luebke@web.de>
2018-07-11 09:19:37 +0200

    Better Main. Spielt damit

--- src/neuralNetwork/MainThilo.java
+++ src/neuralNetwork/MainThilo.java
@@ -23,155 +23,95 @@ public class MainThilo {
 			Network n2=new Network(new ConstantLearningRate(0.001f));//X eher niedrige learning rates (<0.01) scheinen zu funktionieren
 	
 			n2.add(new InputLayer(32*32*3));
-			n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 32*32*3, 16, 16)); 
+			n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 32*32*3, 32*32 /*here*/, 16)); 
 			//X Sigmoid Activation Function ist eine Alternative, wie viele FullyConnected Layer wir brauchen und wie groß sie sind
 			//ist die große Frage!!!
 			
-			
-			
 			//n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 64, 64, 16));
 			//n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 128, 64, 16));
-			n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 16, 16, 16));
+			n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 32*32 /*here*/, 32*16 /*here*/, 16));
+			n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 32*16, 32, 16));
+			n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 32, 16, 16));
 			n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 16, 8, 16));
-			//n2.add(new FullyConnected(new TanhActivation(), new RandomWeight(), new ConstantBias(), 16, 8, 16));
-			//n2.add(new FullyConnected(new SigmoidActivation(), new RandomWeight(), new ConstantBias(), 40, 10));
-			n2.add(new OutputLayer(new EuclideanLoss(), new LinearActivation(), new RandomWeight(), new ConstantBias(), 8, 6, 16));
+			n2.add(new OutputLayer(new EuclideanLoss(), new LinearActivation(), new RandomWeight(), new ConstantBias(), 8/*here*/, 6, 16));
 			
 			//X 6 Outputs muss so bleiben// executionPrevent also der letzte Parameter (16) könnte auch noch verändert werden...
 			
-			int iterations = 500; //X 1/learning rate scheint eine gute Daumenregeln zu sein
+			int iterations = 100; //X 1/learning rate scheint eine gute Daumenregeln zu sein
 			int length3 = dataAndLabel3.size();
-			for(int j=0;j<iterations;j++)
-			{
-				if (j > 10 && j%(iterations/10)==0 || j < 3) System.out.println("Iteration: "+j) ;
-		//early prediction
-				if (j > 99 && j%(iterations/100)==0) {
-					ArrayList<Datum> testData3=DataReader.getImageDataset("image_test1.bin",false);
-					float tempval;
-					float maxtempval = 0;
-					int maxk = 0;
-					for(int i=0;i<testData3.size();i++)
-					{
-						Blob out[]=n2.forward(testData3.get(i).data);
-						for (int k = 0; k < 6; k++) {
-							tempval = out[out.length-1].getValue(k);
-							if (tempval > maxtempval) {
-								maxtempval = tempval;
-								maxk = k;
-							}
-						}
-						testData3.get(i).label.setValue(0, (float) maxk);
-						maxtempval = 0;
-					}
 			
-					DataWriter.writeLabelsToFile("image_prediction.txt", testData3);
-				}
+			SimpleAccuracyFunction af = new SimpleAccuracyFunction();
+			
+			for(int j=1;j<iterations+1;j++)
+			{
+				ArrayList<Float> realError = new ArrayList<Float>();
 				
 				for(int i=0;i<length3;i++)
 				{
-					//if (i%10000 == 0) System.out.println("Test: "+ i + "/" + length3);
+					ArrayList<Float> result = new ArrayList<Float>();
+					ArrayList<Float> expected = new ArrayList<Float>();
+
 					int idx=i;
 					Blob out=n2.trainSimpleSGD(dataAndLabel3.get(idx).data, dataAndLabel3.get(idx).label);
 	
-					if((j==iterations-1 || j<10 || (j%(iterations/100)==0 && j > 50)) && i<10)
+					if(j % 10 == 0)
 					{
 	
 						for(int h=0;h<out.getLength();h++)
 						{
-						 	System.out.print(out.getValue(h)+" ");
+						 	//System.out.print(out.getValue(h)+" ");
+						 	result.add(out.getValue(h));
 						}
 	
-						System.out.print("vs. ");
+						//System.out.print("vs. ");
 	
 						for(int h=0;h<dataAndLabel3.get(idx).label.getLength();h++)
 						{
-						 	System.out.print(dataAndLabel3.get(idx).label.getValue(h)+" ");
+						 	//System.out.print(dataAndLabel3.get(idx).label.getValue(h)+" ");
+						 	expected.add(dataAndLabel3.get(idx).label.getValue(h));
 						}
-						System.out.println();
+						//System.out.println();
+						
+						af.setExpected(expected);
+						af.setResult(result);
+						
+						realError.add(af.computeRealError());
 					}
+				}
+				
+				if(j % 10 == 0)
+				{
+					float sum = 0f;
+					for (Float f : realError)
+					{
+						sum += f;
+					}
+					
+					System.out.println(j + "] real error: " + sum / realError.size());
 				}
 			}
 			
-		//prediction
-			ArrayList<Datum> testData3=DataReader.getImageDataset("image_test1.bin",false);
-			float tempval;
-			float maxtempval = 0;
-			int maxj = 0;
-			for(int i=0;i<testData3.size();i++)
-			{
-				Blob out[]=n2.forward(testData3.get(i).data);
-				for (int j = 0; j < 6; j++) {
-					tempval = out[out.length-1].getValue(j);
-					if (tempval > maxtempval) {
-						maxtempval = tempval;
-						maxj = j;
-					}
-					
-				}
-				testData3.get(i).label.setValue(0, (float) maxj);
-				maxtempval = 0;
-			}
-	
-			DataWriter.writeLabelsToFile("image_prediction.txt", testData3);
-			System.out.print("done");
-		
-
-	
-		
-		
-		
-		
-		
-		/*
-		 * Hier ist der bereich für den Brute Force Algorithmus. Bei diesem dataset nicht zu empfehlen weil es zu lange dauert!!!
-		 */
-		
-		/*
-		SimpleAccuracyFunction saf = new SimpleAccuracyFunction();
-		TestHelper th = new TestHelper(saf);
-		
-		//Define Variables for brute force
-		String nameOfTrainingsData = "image_training.bin";
-		
-		float minLearningRate = 0.0005f, maxLearningRate = 0.001f, learningRateStepSize = 0.0005f;
-		
-		float minStartRate=1, maxStartRate=2, startRateStepSize=1f;
-		
-		int minIterations=500, maxIterations=501, iterationsStepSize=1;
-		
-		float minFactor=1, maxFactor=2, factorStepSize=1;
-		
-		ArrayList<ActivationFunction> activationFunctions = new ArrayList<>();
-		//activationFunctions.add(new LinearActivation());
-		activationFunctions.add(new SigmoidActivation()); //This is the best
-		//activationFunctions.add(new TanhActivation());
-		
-		int minExcecutionPrevent = 16, maxExcecutionPrevent=17, excecutionPreventStepSize=1;
-		
-		int inputCount=32*32*3, outputCountRight=6; //In and out is fix!
-		int minHiddenLayers=2, maxHiddenLayers=6;
-		int minHiddenCount=8, maxHiddenCount=32*32*3;
-		
-		ArrayList<LossFunction> lfList = new ArrayList<>();
-		lfList.add(new EuclideanLoss());
-		
-		ArrayList<WeightFiller> wfList = new ArrayList<>();
-		wfList.add(new RandomWeight());
-		
-		ArrayList<BiasFiller> bfList = new ArrayList<>();
-		bfList.add(new ConstantBias());
-		
-		
-		th.bruteForceTrainingsRun(nameOfTrainingsData, 
-				minLearningRate, maxLearningRate, learningRateStepSize, 
-				minStartRate, maxStartRate, startRateStepSize, 
-				minIterations, maxIterations, iterationsStepSize, 
-				minFactor, maxFactor, factorStepSize, activationFunctions, 
-				inputCount, outputCountRight, 
-				/* minOutputCountLeft, maxOutputCountLeft,*/ /*minHiddenLayers, maxHiddenLayers, minHiddenCount, maxHiddenCount, 
-				lfList, wfList, bfList,
-				minExcecutionPrevent, maxExcecutionPrevent, excecutionPreventStepSize);
-
-				*/
+			//prediction
+//			ArrayList<Datum> testData3=DataReader.getImageDataset("image_test1.bin",false);
+//			float tempval;
+//			float maxtempval = 0;
+//			int maxj = 0;
+//			for(int i=0;i<testData3.size();i++)
+//			{
+//				Blob out[]=n2.forward(testData3.get(i).data);
+//				for (int j = 0; j < 6; j++) {
+//					tempval = out[out.length-1].getValue(j);
+//					if (tempval > maxtempval) {
+//						maxtempval = tempval;
+//						maxj = j;
+//					}
+//					
+//				}
+//				testData3.get(i).label.setValue(0, (float) maxj);
+//				maxtempval = 0;
+//			}
+//	
+//			DataWriter.writeLabelsToFile("image_prediction.txt", testData3);
+//			System.out.print("done");
 	}
 }
